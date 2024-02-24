3e70cbd71dca8f10d2b1ece3445ef099dd83de86
Davi Souza Santos <davii.clink@gmail.com>
2024-02-24 02:09:12 -0300

    portfólio iniciado(piloto)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import perfil from './assets/images/perfil.png'
 
-function App() {
+export default function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className='page'>
+      <section className='page-presentation'>
+        <img className='image' src={perfil} alt='' />
+        
+        <div id='presentation'>
+            <p className='presentationText'>
+            Olá, me chamo Davi e sou um DEV júnior <span className='hightexts'> fullstack. </span> Atualmente estou me especializando em <span className='hightexts'>programação web </span>com <span className='hightexts'>react</span>.
+            </p>
+            
+            <p className='presentationText'>
+            Busco minha primeira oportunidade no mercado de programação, onde quero seguir minha carreira.
+            </p>
+
+            <p className='presentationText'>
+            As tecnologias que utilizo são:
+            </p>
+            
+            <div className='technologies'>
+                <img className='technology' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" alt='react' />
+              
+                <img className='technology' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" />
+                
+                <img className='technology' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg" />
+              
+                <img className='technology' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+                  
+               <img className='technology' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+           
+              <img className='technology' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" />
+          
+            </div>
+        </div>
+      </section>   
+    </body>
   );
 }
-
-export default App;
